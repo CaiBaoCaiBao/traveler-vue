05f0306f5e62c90f396287c26f60998109d0c299
CaiBaoCaiBao <13503089560@163.com>
2025-06-12 21:42:08 +0800

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# .
+# PAFTL-Admin System
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import HeaderComponent from '@/components/HeaderComponent/HeaderComponent.vue';
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header><HeaderComponent/></el-header>
+      <el-main>
+        <RouterView />
+      </el-main>
+    </el-container>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
