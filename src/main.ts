1ef080ccbd966ce8bed06823ee020f77b8f66a41
CaiBaoCaiBao <13503089560@163.com>
2025-06-13 09:28:37 +0800

    style:add new css file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './assets/main.scss'
+import '@/assets/main.scss'
 
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from './router'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 const app = createApp(App)
 
