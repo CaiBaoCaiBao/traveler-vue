af8cf9468be253959a4e5ddcc678a1b2a6c15a29
CaiBaoCaiBao <13503089560@163.com>
2025-06-14 02:27:59 +0800

    style:container for loading all parts of scenic area management

---
+++ src/views/AttractionView/AttractionView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="attraction-view">
+        <SideBar />
+        <main id="main-content">
+            <BaseInfoSection class="section-group" />
+            <TicketSection class="section-group" />
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SideBar from '@/views/AttractionView/SideBar.vue';
+import BaseInfoSection from '@/views/AttractionView/Sections/BaseInfoSection.vue';
+import OrderSection from '@/views/AttractionView/Sections/OrderSection.vue';
+import ReservationSection from '@/views/AttractionView/Sections/ReservationSection.vue';
+import TicketSection from '@/views/AttractionView/Sections/TicketSection.vue';
+</script>
+
+<style scoped lang="scss">
+@use 'style/index.scss';
+</style>
