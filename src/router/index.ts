76d11d019fd743266127998b62e892e60a8794b2
CaiBaoCaiBao <13503089560@163.com>
2025-06-13 09:47:26 +0800

    feat:add two page router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView/HomeView.vue'
-
+import HomeView from '@/views/HomeView/HomeView.vue'
+import AboutView from '@/views/AboutView/AboutView.vue'
+import AttractionView from '@/views/AttractionView/AttractionView.vue'
+import LoginView from '@/views/LoginView/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,8 +17,18 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView/AboutView.vue'),
+      component:AboutView,
     },
+    {
+      path: '/attraction',
+      name: 'attraction',
+      component: AttractionView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    }
   ],
 })
 
